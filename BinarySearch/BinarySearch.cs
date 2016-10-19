d30983623c8c8903c70e1f14c08ff07448fa97b6
Vistor25 <victorvoronko@gmail.com>
2016-10-19 23:45:26 +0300

    Task1

---
+++ BinarySearch/BinarySearch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Logic
+{
+    public class BinarySearch
+    {
+        public static int BinarySearch_Iter(int[] array, int key)
+        {
+            int left = 0;
+            int right = array.Length;
+
+            while (!(left >= right))
+            {
+                int mid = left + (right - left) / 2;
+
+                if (array[mid] == key)
+                    return mid;
+
+                if (array[mid] > key)
+                    right = mid;
+                else
+                    left = mid + 1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Logic;
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность массива:");
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(0, 1000);
+            }
+            Array.Sort(array);
+            Console.WriteLine("Введите искомый элемент: ");
+            int element = int.Parse(Console.ReadLine());
+            Console.WriteLine("Индекс элемента:{0}", BinarySearch.BinarySearch_Iter(array, element));
+            Console.ReadLine();
+        }
+    }
+}
